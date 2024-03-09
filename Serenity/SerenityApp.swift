54de44105873dac2f3d7d22f80c24033921a2e65
Mello Birkan <mgbirkan@icloud.com>
2024-03-08 23:37:45 -0300

    Initial Commit

---
+++ Serenity/SerenityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerenityApp.swift
+//  Serenity
+//
+//  Created by Marcello Gonzatto Birkan on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SerenityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
